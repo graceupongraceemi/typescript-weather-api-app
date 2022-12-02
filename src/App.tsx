871356c145d1e4da7cf13fe4d5ce3a7b1f361365
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-01 21:43:53 -0800

    weather forecast search section basic styling done in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      Hello world
+      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:-x-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700">
+        Hello World
+      </section>
     </main>
   )
 }
