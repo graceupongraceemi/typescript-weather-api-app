3e2b2f4ac8dd5173422fe6205e3c7c07bb66063e
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-02 21:15:38 -0800

    In App.tsx, by importing useState and ChangeEvent, getSearchOptions function that fetch api data as well as onInputChange function to retrive value is defined

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,30 @@
-const App = () => {
+import { useState, ChangeEvent } from 'react'
+
+const App = (): JSX.Element => {
+  const [term, setTerm] = useState<string>('')
+  const [options, setOptions] = useState<[]>([])
+
+  const getSearchOptions = (value: string) => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
+        process.env.REACT_APP_API_KEY
+      }`
+    )
+      .then((res) => res.json())
+      .then((data) => setOptions(data))
+  }
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim()
+    setTerm(value)
+
+    if (value === '') return
+
+    getSearchOptions(value)
+  }
+
+  // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
       <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:-x-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700">
@@ -14,9 +40,14 @@ const App = () => {
         <div className="flex mt-10 md:mt-4">
           <input
             type="text"
-            value={''}
+            value={term}
             className="px-2 py-1 rounded-1-md border-2 border-white"
+            onChange={onInputChange}
           />
+
+          {options.map((option: { name: string }) => (
+            <p>{option.name}</p>
+          ))}
 
           <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500  text-zinc-100 px-2 py-1 cursor-pointer">
             Search
