7a116a16a4167fc67b559bb19aa73baefa6085ff
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-03 18:16:02 -0800

    useForecast.ts created to process fetch forecast functionality which moved out of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,9 @@
-import { useState, ChangeEvent, useEffect } from 'react'
 import Search from './components/Search'
-
-import { optionType } from './types'
+import useForecast from './hooks/useForecast'
 
 const App = (): JSX.Element => {
-  const [term, setTerm] = useState<string>('')
-  const [city, setCity] = useState<optionType | null>(null)
-  const [options, setOptions] = useState<[]>([])
-  const [forecast, setForecast] = useState<null>(null)
-
-  const getSearchOptions = (value: string) => {
-    fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
-        process.env.REACT_APP_API_KEY
-      }`
-    )
-      .then((res) => res.json())
-      .then((data) => setOptions(data))
-  }
-
-  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.trim()
-    setTerm(value)
-
-    if (value === '') return
-
-    getSearchOptions(value)
-  }
-
-  const getForecast = (city: optionType) => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
-    )
-      .then((res) => res.json())
-      .then((data) => setForecast(data))
-  }
-
-  const onSubmit = () => {
-    if (!city) return
-
-    getForecast(city)
-  }
-
-  const onOptionSelect = (option: optionType) => {
-    setCity(option)
-  }
-
-  useEffect(() => {
-    if (city) {
-      setTerm(city.name)
-      setOptions([])
-    }
-  }, [city])
-
-  // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+  const { term, options, forecast, onInputChange, onOptionSelect, onSubmit } =
+    useForecast()
 
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
