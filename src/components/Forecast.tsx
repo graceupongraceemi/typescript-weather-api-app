73a6726f65e9a0c1332f211eed30b1305165b62d
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-05 12:04:25 -0800

    In Forecast.tsx component, added styling for the main div and div to hold section

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -6,8 +6,12 @@ type Props = {
 
 const Forecast = ({ data }: Props): JSX.Element => {
   return (
-    <div>
-      <p>Forecast</p>
+    <div className="w-full md:max-w-[500px] py-4 md:py-4 md:px-10 lg:p-24 h-full lg:h-auto bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg">
+      <div className="mx-auto w-[300px]">
+        <section className="text-center">
+          <h2>{data.name}</h2>
+        </section>
+      </div>
     </div>
   )
 }
