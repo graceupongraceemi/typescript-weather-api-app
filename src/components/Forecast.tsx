7c325b744c8466ce7af8c34ea13e298944ddd926
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-05 14:46:23 -0800

    data list section in Forecast.tsx component updated with list of various weather icons that shows the time and the temperature with scroll bar

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -34,6 +34,25 @@ const Forecast = ({ data }: Props): JSX.Element => {
             L: <Degree temp={Math.floor(today.main.temp_min)} />
           </p>
         </section>
+        <section className="flex overflow-x-scroll mt-4 pb-2 mb-5">
+          {data.list.map((item, i) => (
+            <div
+              className="inline-block text-center w-[50px] flex-shrink-0"
+              key={i}
+            >
+              <p className="text-sm">
+                {i === 0 ? 'Now' : new Date(item.dt * 1000).getHours()}
+              </p>
+              <img
+                alt={`weather-icon-${item.weather[0].description}`}
+                src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+              />
+              <p className="text-sm font-bold">
+                <Degree temp={Math.round(item.main.temp)} />
+              </p>
+            </div>
+          ))}
+        </section>
       </div>
     </div>
   )
